d71d1eee7587dfbab40da31f2efbee779147dd0d
JMC50 <minchan50@naver.com>
2025-06-30 04:16:20 +0900

    add base config files

---
+++ backend/src/config/config.ts
@@ -0,0 +1,7 @@
+export const private_key = "your-secret-key";
+export const admin_passowrd = "your-admin-password";
+export const PORT = 7777;
+export const diskPath = "/";
+export const KAKAO_REST_API_KEY = "your-kakao-api-key";
+export const KAKAO_REDIRECT_URL = "https://your-frontend-url/kakaoLogin";
+export const KAKAO_CLIENT_SECRET = "your-kakao-client-secret"
